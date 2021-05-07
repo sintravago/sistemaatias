cb24ac85b5c3cf326dcdfa482b78fca21612da88
Victor Graterol <46805250+sintravago@users.noreply.github.com>
2021-05-07 15:42:22 -0400

    islr

--- pagos/admin.py
+++ pagos/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import factura
+from .models import factura, Islr
 # Register your models here.
 
 class facturaAdmin(admin.ModelAdmin):
     list_display = ('empresa','big','exento')
 
+class IslrAdmin(admin.ModelAdmin):
+    list_display = ('codigo','actividad','porcentaje','sustraendo')
+
 admin.site.register(factura, facturaAdmin)
+admin.site.register(Islr, IslrAdmin)
