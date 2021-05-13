cc8b1c783a752b2c320d8a4af5efa472e58e1604
Victor Graterol <46805250+sintravago@users.noreply.github.com>
2021-05-13 15:14:16 -0400

    cambio

--- horario/settings.py
+++ horario/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a)3)jy4enbj0n2#ka7*6@@xy7br$j)1=)0455x(*=x+_tlvxv2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
